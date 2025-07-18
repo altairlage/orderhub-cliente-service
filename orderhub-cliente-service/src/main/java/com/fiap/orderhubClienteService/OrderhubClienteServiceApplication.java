a5045d7bf3e184267ad882cfe7353f4bc5cb3a25
Vitor Cometti <vitor.cometti@ntconsult.com.br>
2025-07-18 14:46:15 -0300

    correcao

--- orderhub-cliente-service/src/main/java/com/fiap/orderhubClienteService/OrderhubClienteServiceApplication.java
+++ orderhub-cliente-service/src/main/java/com/fiap/orderhubClienteService/OrderhubClienteServiceApplication.java
@@ -1,13 +1,13 @@
-package com.fiap.orderhubClientService;
+package com.fiap.orderhubClienteService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OrderhubClientServiceApplication {
+public class OrderhubClienteServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OrderhubClientServiceApplication.class, args);
+		SpringApplication.run(OrderhubClienteServiceApplication.class, args);
 	}
 
 }
